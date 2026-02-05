0280d71e35ae6fbed839435a6f2d14e1a62f6af2
Sanjeev Midda <s.midda1@gmail.com>
2026-02-05 23:52:07 +0000

    added dynamic color property into Habit component

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -14,7 +14,10 @@ const Habit = ({
   return (
     <div className="habitContainer" onClick={() => updateAmount(keyValue)}>
       <p>{name}</p>
-      <div className="circle">
+      <div
+        className="circle"
+        style={{ background: `linear-gradient(45deg, ${color}, ${color})` }}
+      >
         <p>{amount}</p>
       </div>
     </div>
