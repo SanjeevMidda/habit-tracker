a94b917db0dbebd6034eea81fa591083a481d193
Sanjeev Midda <s.midda1@gmail.com>
2026-02-05 20:33:51 +0000

    stored basic habit in habits variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./index.css";
 
 function App() {
+  // current habits
+  const habits = [
+    {
+      name: "drink water",
+      amount: 0,
+    },
+  ];
+
+  // input to add a habit
+  //habit has a color, choice to increase or decrease and name
+  //habit can be removed
+  // Shows total number of habits
+
   return <div className="App"></div>;
 }
 
